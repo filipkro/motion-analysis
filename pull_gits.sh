034eb4b5ba3a27052b5424cd00b8ff6659866d5f
filipkro <filip.kronstrom@gmail.com>
2020-10-22 13:23:37 +0200

    from cluster

--- pull_gits.sh
+++ pull_gits.sh
@@ -1,11 +1,5 @@
 # TODO: fix with -C flag and something workingfor git 1.8...
-dir="$(pwd)"
 git pull
-cd pose/mmpose
-git pull
-cd mmdetection
-git pull
-cd $dir
-cd tsc/dl-4-tsc
-git pull
-cd $dir
+git -C pose/mmpose pull
+git -C pose/mmpose/mmdetection pull
+git -C tsc/dl-4-tsc pull
