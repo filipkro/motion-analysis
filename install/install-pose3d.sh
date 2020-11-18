7bc8ef097444ba3fa7ec7561fbb2abaf4237c8e0
filipkro <filip.kronstrom@gmail.com>
2020-11-18 09:12:04 +0100

    launch scripts from cluster

--- install/install-pose3d.sh
+++ install/install-pose3d.sh
@@ -1,14 +1,14 @@
 pip install torchvision==0.5.0 # make sure compatible with pytorch on cluster
-pip install mmcv-full==1.1.5
+#pip install mmcv-full
 
 pip install -r install/requirements-cluster.txt
 
-cd pose/mmpose/mmdetection
-pip install -v -e .
+#cd pose/mmpose/mmdetection
+#pip install -v -e .
 
-cd ..
+#cd ..
 
 #install mmpose:
-python setup-cluster.py develop
+#python setup-cluster.py develop
 
-cd ../..
+#cd ../..
