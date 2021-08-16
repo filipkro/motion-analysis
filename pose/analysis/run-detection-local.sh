b520ab81f75b941e1c6a3c4e6804a90cbd0b49ff
filipkro <filip.kronstrom@gmail.com>
2021-08-16 16:20:29 +0200

    coco weholebody fix in script

--- pose/analysis/run-detection-local.sh
+++ pose/analysis/run-detection-local.sh
@@ -3,8 +3,8 @@
 TMPDIR="/home/filipkr/Documents/xjob"
 ### TODO: fix file structure and script to automatically change config and checkpoint
 ### TODO: automate detection of all videos
-MODEL_CHECKPOINT="$TMPDIR/motion-analysis/pose/mmpose/checkpoints/top-down/hrnet_w48_coco_384x288_dark-741844ba_20200812.pth"
-MODEL_CONFIG="$TMPDIR/motion-analysis/pose/mmpose/configs/top_down/darkpose/coco/hrnet_w48_coco_384x288_dark.py"
+MODEL_CHECKPOINT="$TMPDIR/motion-analysis/pose/mmpose/checkpoints/top-down/hrnet_w48_coco_wholebody_384x288_dark-f5726563_20200918.pth"
+MODEL_CONFIG="$TMPDIR/motion-analysis/pose/mmpose/configs/top_down/darkpose/coco-wholebody/hrnet_w48_coco_wholebody_384x288_dark.py"
 #MODEL_CHECKPOINT="$TMPDIR/motion-analysis/pose/mmpose/checkpoints/top-down/hrnet_w48_coco_256x192_dark-8cba3197_20200812.pth"
 #MODEL_CONFIG="$TMPDIR/motion-analysis/pose/mmpose/configs/top_down/darkpose/coco/hrnet_w48_coco_256x192_dark.py"
 FOLDER_BOX="$TMPDIR/motion-analysis/pose/mmpose/mmdetection/"
