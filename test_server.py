571ef87cffcd3ca426b9c4c6691690e8bc8dbb8f
filipkro <filip.kronstrom@gmail.com>
2021-08-17 21:07:54 +0200

    test for correct file

--- test_server.py
+++ test_server.py
@@ -10,15 +10,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!\nLOL</p>"
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    args = Namespace()
-    # args.video = '/home/filipkr/Documents/xjob/vids/real/Videos/MUSSE/musse-SLS/03SLS1R_MUSSE.mts'
-    args.video = '/code/03SLS1R_MUSSE.mts'
-    args.out = 'out3'
-    # run_inference(args)
-    run_inference()
-    print("in pred")
+    # args = Namespace()
+    # # args.video = '/home/filipkr/Documents/xjob/vids/real/Videos/MUSSE/musse-SLS/03SLS1R_MUSSE.mts'
+    # args.video = '/code/03SLS1R_MUSSE.mts'
+    # args.out = 'out3'
+    # # run_inference(args)
+    # run_inference()
+    # print("in pred")
     return "<p>lol</p>"
